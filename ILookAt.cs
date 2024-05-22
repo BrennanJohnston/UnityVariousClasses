8e759758d3100920d7d3ce791113aa5ef01b2077
BrennanJohnston <brennankj@live.com>
2024-05-22 14:00:02 -0500

    Curated after picking random classes

--- ILookAt.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface ILookAt {
-    bool LookingAt { get; }
-    Vector3 LookingAtPosition { get; }
-    void LookAt(Vector3 position);
-}
--- IMoveTo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IMoveTo {
-    bool MovingTo { get; }
-    Vector3 MovingToPosition { get; }
-    void MoveTo(Vector3 position);
-}
